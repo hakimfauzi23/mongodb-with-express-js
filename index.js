d63fb40947027f1f10d78cf194d0c8e4298c8894
Hanif <hanif.hakim@spesolution.com>
2022-11-21 21:43:45 +0700

    Setup mongodb

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+// Connect MongoDB to server.
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
 
 const app = express();
 
@@ -7,5 +22,3 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
 });
-
-
